8cdff718515302bb10422e1538eeac21cd707465
la1av1a <zldqkrktqk@gmail.com>
2022-12-11 13:46:07 +0900

    feat : corsConfig

--- src/main/java/com/example/boardboard/web/BoardController.java
+++ src/main/java/com/example/boardboard/web/BoardController.java
@@ -11,12 +11,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 public class BoardController {
--- src/main/java/com/example/boardboard/web/MapController.java
+++ src/main/java/com/example/boardboard/web/MapController.java
@@ -5,9 +5,11 @@ import com.example.boardboard.web.dto.MapResponseDTO;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @RestController
 public class MapController {
